fd42f1c94f310cd8f2ec1848833b5d97e0c8c08b
3276908088 <3276908088@qq.com>
2023-09-17 20:51:22 +0800

    Second Commit

--- src/main/java/com/ithema/git/GitTest.java
+++ src/main/java/com/ithema/git/GitTest.java
@@ -3,5 +3,6 @@ package com.ithema.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello GIT!!!");
+        System.out.println("hello GIT2!!!");
     }
 }
